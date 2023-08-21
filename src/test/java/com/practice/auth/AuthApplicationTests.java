52ea63d09b8394f96e030c28b5bbbaf907fd48d0
tableminpark <tablemin_park@daum.net>
2023-08-22 03:31:55 +0900

    init : 프로젝트 환경 구축

---
+++ src/test/java/com/practice/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
