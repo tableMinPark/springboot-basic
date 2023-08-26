89e813fde92af666ac77fa185232fd39c505bf8d
tableminpark <tablemin_park@daum.net>
2023-08-26 21:59:50 +0900

    refac : 회원 가입 입력 값 dto 형태로 수정

--- src/main/java/com/practice/auth/controller/AuthController.java
+++ src/main/java/com/practice/auth/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.practice.auth.controller;
 
+import com.practice.auth.dto.request.RegisterMemberReqDto;
 import com.practice.auth.global.code.FailCode;
 import com.practice.auth.global.exception.FailException;
 import com.practice.auth.global.response.SuccessResponse;
@@ -8,10 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -21,9 +19,11 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/register")
-    public ResponseEntity<Object> registerMember(@RequestParam(name = "email") String email,
-                                                 @RequestParam(name = "password") String password) throws RuntimeException {
+    public ResponseEntity<Object> registerMember(@RequestBody RegisterMemberReqDto registerMemberReqDto) throws RuntimeException {
         log.info("registerMember - Call");
+
+        String email = registerMemberReqDto.getEmail();
+        String password = registerMemberReqDto.getPassword();
 
         if (email == null || password == null) {
             throw new FailException(FailCode.INVALID_ARGS);
