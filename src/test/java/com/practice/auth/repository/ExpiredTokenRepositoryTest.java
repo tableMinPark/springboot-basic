a5159eee3ee30ffcbfd37404784909ab486332ea
tableminpark <tablemin_park@daum.net>
2023-08-27 00:25:28 +0900

    test : expiredToken 조회 단위 테스트

--- src/test/java/com/practice/auth/repository/ExpiredTokenRepositoryTest.java
+++ src/test/java/com/practice/auth/repository/ExpiredTokenRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.practice.auth.repository;
 
 import com.practice.auth.entity.ExpiredToken;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,18 +24,22 @@ class ExpiredTokenRepositoryTest {
     @Autowired
     private ExpiredTokenRepository expiredTokenRepository;
 
-    private final String accessToken = "accessToken_sample";
-    private final String refreshToken = "refreshToken_sample";
+    private final String TOKEN = "token_sample";
     private final Long MEMBER_ID = 1L;
 
     @BeforeEach
     void registerExpiredToken() {
         ExpiredToken expiredToken = ExpiredToken.builder()
-                .token(accessToken)
+                .token(TOKEN)
                 .memberId(1L)
                 .build();
 
         expiredTokenRepository.save(expiredToken);
+    }
+
+    @AfterEach
+    void deleteExpiredToken() {
+        expiredTokenRepository.deleteById(TOKEN);
     }
 
     @DisplayName("ExpiredToken 생성 테스트")
@@ -40,5 +47,18 @@ class ExpiredTokenRepositoryTest {
     @Transactional
     void expiredTokenRegisterTest() {
         assertTrue(true);
+    }
+
+
+    @DisplayName("ExpiredToken 조회 테스트")
+    @Test
+    @Transactional
+    void expiredTokenFindTest() {
+        Optional<ExpiredToken> op = expiredTokenRepository.findById(TOKEN);
+        assertTrue(op.isPresent());
+
+        ExpiredToken expiredToken = op.get();
+        assertEquals(TOKEN, expiredToken.getToken());
+        assertEquals(MEMBER_ID, expiredToken.getMemberId());
     }
 }
