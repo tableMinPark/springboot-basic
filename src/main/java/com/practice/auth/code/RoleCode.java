1ca48be1c641addeb5043620cdceb3b03738358a
tableminpark <tablemin_park@daum.net>
2023-08-26 14:44:13 +0900

    feat : 회원 구분 코드 enum 생성

---
+++ src/main/java/com/practice/auth/code/RoleCode.java
@@ -0,0 +1,13 @@
+package com.practice.auth.code;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum RoleCode {
+    NORMAL("NORMAL", "일반 회원"),
+    COMPANY("COMPANY", "기업 회원"),
+    ADMIN("ADMIN", "관리자");
+
+    public final String code;
+    public final String name;
+}
